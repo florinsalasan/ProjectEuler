485d219d4d3d7ffed830bc4001d3b08358d27205
Florin Salasan <florinsalasan@gmail.com>
2019-07-22 15:53:37 -0400

    need to debug this on personal computer with node debugger

--- 005-SmallestMultiple/solution.js
+++ 005-SmallestMultiple/solution.js
@@ -11,3 +11,37 @@ function primeFactors(ofThis) {
     }
     return multiples;
 }
+
+function smallestMultiple(upTo) {
+    var largestNeeded = new Map();
+    var collection = [];
+    for (i = upTo; i > 1; i--) {
+        var toMap = primeFactors(i);
+        collection.push(toMap);
+    }
+    for (i = collection.length; i > 1; i--) {
+        var j = 0;
+        while (j < toMap.length) {
+            var currentFactor = toMap[j];
+            var numOfCurFactor = 1;
+            while (toMap[j] === toMap[j + numOfCurFactor]) {
+                numOfCurFactor++;
+            }
+            if (largestNeeded.has(currentFactor)) {
+                if (largestNeeded.get(currentFactor) < numOfCurFactor){
+                    largestNeeded.set(currentFactor, numOfCurFactor);
+                }
+            }else{
+                largestNeeded.set(currentFactor, numOfCurFactor);
+            }
+            j += numOfCurFactor;
+            numOfCurFactor = 1;
+        }
+    }
+    var total = 1;
+    for (const key of largestNeeded.keys()) {
+        total *= (key * largestNeeded[key]);
+    }
+    return total;
+
+}
