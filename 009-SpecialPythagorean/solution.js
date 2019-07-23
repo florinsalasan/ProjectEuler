2f3a59a736434c6192a84d38ba8950924953d0c7
Florin Salasan <florinsalasan@gmail.com>
2019-07-23 15:24:30 -0400

    idea for problem 9 solution

---
+++ 009-SpecialPythagorean/solution.js
@@ -0,0 +1,7 @@
+// Use fibonacci's method to generate primitive triples until one is found that can divide perimeter,
+// Scale the sides by the quotient obtained by dividing perimeter by sum of sides from primitive triple
+// return the product of the scaled sides.
+
+function perimeterToProduct(perimeter) {
+    
+}
