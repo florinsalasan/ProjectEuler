504303acc62807d4452f4d731fb31f62bbd246c1
Florin Salasan <florinsalasan@gmail.com>
2019-09-12 17:32:07 -0400

    have the skeleton of the algo going, however it is only returning the value passed into it, need to look at while and for loop to see if it is being reset somewhere

--- 014-LongestCollatz/solution.js
+++ 014-LongestCollatz/solution.js
@@ -8,7 +8,41 @@ function longestCollatz(startingLimit) {
     //each number in the sequence to see if the length is written in the Map, at which point the value from the map will be added to the determined length of the previous 
     //items in the sequence and each one will be added to the Map. Seems like it might be rather space inefficient before starting. Thus on a second run, starting at 10, 
     //the 10 will be in the Map along with the length of its sequence, hopefully reducing the number of calculations that are necessary.
-    for (var i = startingLimit; i > 0; i--){
-        
+    var valueWithLargestChain = startingLimit;
+    var largestChainLength = 1;
+    for (var i = startingLimit; i > 0; i--) {
+        var valuesSeenSoFar = [];
+        var toGetToOne = i;
+        valuesSeenSoFar.concat(toGetToOne)
+        while (toGetToOne != 1) {
+            if (!pastSequences.has(toGetToOne)) {
+                //the check if the value is already in the map, continues the sequence
+                if (toGetToOne % 2 == 0) {
+                    toGetToOne /= 2;
+                    valuesSeenSoFar.concat(toGetToOne);
+                } else {
+                    toGetToOne = 3 * toGetToOne + 1;
+                    valuesSeenSoFar.concat(toGetToOne);
+                }
+            } else {
+                break;
+            }
+        }
+        if (toGetToOne !== 1) {
+            for (var toSubtract = 0; toSubtract < valuesSeenSoFar.length; toSubtract++) {
+                //Loop adds the length of the sequence of toGetToOne to the length of all values that had been gone through
+                pastSequences.set(valuesSeenSoFar[i], valuesSeenSoFar.length - toSubtract + pastSequences.get(toGetToOne));
+            }
+        } else {
+            //The loop needed to add ALL values to the map since none of them have been seen and thus the while loop was never cut early:
+            for (var toSubtract = 0; toSubtract < valuesSeenSoFar.length; toSubtract++) {
+                pastSequences.set(valuesSeenSoFar[i], valuesSeenSoFar.length - toSubtract);
+            }
+        }
+        if (valuesSeenSoFar.length > largestChainLength) {
+            largestChainLength = valuesSeenSoFar.length;
+            valueWithLargestChain = i;
+        }
     }
+    return valueWithLargestChain;
 }
